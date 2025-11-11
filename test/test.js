74496d2eeed87613dde37922d7678e59b84cfeac
ChayLim <156899992+ChayLimm@users.noreply.github.com>
2025-11-11 14:48:42 +0700

    test pr

--- test/test.js
+++ test/test.js
@@ -8,5 +8,6 @@ describe('GET /', () => {
         const res = await request(app).get('/');
         expect(res.status).to.equal(200);
         expect(res.text).to.equal('Hello, CI/CD!');
+        ///testing
     });
 });
